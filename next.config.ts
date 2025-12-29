9c04dfeb0288a17dcf6ccb70ad4886136d908c71
Amr Khaled <143969993+amr0khaled@users.noreply.github.com>
2025-12-29 19:26:01 +0200

    Revert "Feature/api/db (#3)" (#4)
    This reverts commit a6aa3f657f60ac482b5ebb37ec5e6fa2645769ae.

--- next.config.ts
+++ next.config.ts
@@ -3,25 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com",
-              "frame-src 'self' https://*.firebaseapp.com https://accounts.google.com",
-              "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com",
-              "style-src 'self' 'unsafe-inline'"
-            ].join('; ')
-          }
-        ]
-      }
-    ]
-  }
-}
+};
 
 export default nextConfig;
