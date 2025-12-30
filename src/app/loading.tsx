20eb222eabe00cc25b9f027fa11588be299a9312
Amr Khaled <redwolf4325@gmail.com>
2025-12-30 19:33:27 +0200

    Added Hero section Loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from "@/components/ui/spinner";
+
+
+export default function Loading() {
+  return <main className='flex justify-center items-center min-h-screen w-full'>
+    <Spinner />
+  </main>
+}
