977f17f9870b4d68fa5fe538fc6a5a09d9e93416
Amr Khaled <redwolf4325@gmail.com>
2025-12-30 19:33:27 +0200

    Added Hero section Loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import '@/styles/pages/home.css'
 
 export default function Home() {
-  return (
-    <main>
-    </main>
-  );
+  return <section className='hero'>
+    <p className='main-paragraph'>
+      <span className='your'>Your</span> Next tool for Writer's block<br />
+      AI Content Creation Tool
+    </p>
+    <div className='operation-buttons'>
+      <Button className='bg-secondary text-accent'>Learn more</Button>
+      <Button className="bg-accent text-secondary">Get Started</Button>
+    </div>
+  </section>;
 }
