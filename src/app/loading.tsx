b5ca21ce237d60f63f95a8fc721b11677d3ca48f
Amr Khaled <redwolf4325@gmail.com>
2026-01-04 22:09:52 +0200

    Added debounce to handle the fast changing in input field before API call Added Axios object to standarize the requests with better handling Modified Axios request interceptors to add the user id api calling But did not handle the post creation in backend ** This will be the next TODO Modified the ui a little bit Added React Markdown with Remark GFM to turn AI response (in markdown) into JSX elements (in future I may add debouncer to ai response) Removed loading border

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,6 +3,6 @@ import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
   return <section className='bg-transparent flex justify-center mx-auto my-auto items-center min-h-[calc(100vh-80px)] w-full pt-20'>
-    <Spinner className='size-12 border border-white' />
+    <Spinner className='size-12' />
   </section>
 }
