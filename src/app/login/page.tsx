569ff1dc7874b71843b2bfc729a25d42cd834430
Amr Khaled <redwolf4325@gmail.com>
2026-01-19 20:03:42 +0200

    removed unused import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,7 +16,6 @@ import { toast } from "sonner";
 import { Separator } from "@/components/ui/separator";
 import { FaGoogle } from "react-icons/fa6";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import debounce from 'lodash/debounce'
 
 enum SignInMethod {
   EMAIL = "EMAIL",
