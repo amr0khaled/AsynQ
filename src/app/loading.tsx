a52efced6c2f667276381375bb72199e04ededf8
Amr Khaled <redwolf4325@gmail.com>
2026-01-19 20:02:09 +0200

    Fixed merge conflict

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,13 +2,9 @@ import { Spinner } from "@/components/ui/spinner";
 
 
 export default function Loading() {
-<<<<<<< Updated upstream
-  return <section className='flex justify-center mx-auto my-auto items-center min-h-[calc(100vh-80px)] w-full pt-20'>
-=======
   return (
 
     //<section className='bg-transparent flex flex-col justify-center mx-auto my-auto items-center min-h-[calc(100vh-80px)] w-full pt-20'>
->>>>>>> Stashed changes
     <Spinner className='size-12' />
     //</section>
   )
