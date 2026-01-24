788f514ebf361333b74b4cdd9626cb6680e4dcc3
Amr Khaled <redwolf4325@gmail.com>
2026-01-24 04:16:48 +0200

    Fixed some stuff for building

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,18 +7,21 @@ import { Prisma } from '@/lib/prisma/client'
 async function user() {
   const users: Prisma.UserCreateInput[] = [
     {
+      id: "12",
       name: "Local",
       email: "local@local.com",
     },
     {
+      id: "1",
       name: "Local2",
       email: "local2@local.com",
     },
   ]
-  for (const { name, email } of users) {
+  for (const { name, email, id } of users) {
     if (!(await prisma.user.findFirst({ where: { email } }))) {
       await prisma.user.create({
         data: {
+          id,
           name,
           email
         }
